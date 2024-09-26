3664f6a8623b336791737b9b4656994d743bbf6b
Ma. Catherine Sanchez <118100769+CatherineCadano@users.noreply.github.com>
2024-09-26 23:37:17 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+
+
+import React from 'react';
+import AdminSignup from './components/Admin';
+
+
+function App() {
+  return (
+    <div>
+      <AdminSignup />
+    </div>
+  );
+}
+
+
+export default App;
